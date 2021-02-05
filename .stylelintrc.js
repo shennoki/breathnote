368d7e2150d7d443708f720878959f123ee96453
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-02-05 23:45:22 +0900

    chore: Add auxiliary tools (TypeScript, ESLint, stylelint, Prettier, EditorConfig)

---
+++ .stylelintrc.js
@@ -0,0 +1,6 @@
+/* stylelint | https://stylelint.io/user-guide/configure */
+
+module.exports = {
+  /* 公式のリントルールを適用 */
+  extends: ['stylelint-config-recommended-scss', 'stylelint-config-prettier'],
+}
