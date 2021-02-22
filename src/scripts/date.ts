bdc8a355076847357c1526aab9b10e181e27a3de
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-02-22 16:31:02 +0900

    fix: Add date exception handling in preview mode

--- src/scripts/date.ts
+++ src/scripts/date.ts
@@ -2,6 +2,11 @@ import { format } from 'date-fns'
 import { utcToZonedTime } from 'date-fns-tz'
 
 export const getCorrectDate = (utcDate: string, string?: string): string => {
+  /* プレビュー時など日付が設定されていない場合の処理 */
+  if (!utcDate) {
+    return '日付を取得できません'
+  }
+
   const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo')
 
   if (!string) return jstDate.toString()
