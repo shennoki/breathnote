7e1e5d4a40dd00a5334e6cbfaff728ab5b1383dd
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-02-23 22:42:04 +0900

    style: Tweak the style of the article

--- src/pages/posts/[slug]/index.tsx
+++ src/pages/posts/[slug]/index.tsx
@@ -64,9 +64,9 @@ const Post: NextPage<Props> = ({ config, option, post }) => {
         </noscript>
       </Head>
       <Body pageType={option.pageType} fullPath={option.fullPath}>
-        <article className="px-3 sm:px-5 md:px-7 lg:px-16 pt-5 sm:pt-6 md:pt-7 lg:pt-14 pb-8 sm:pb-9 md:pb-12 lg:pb-20 mb-5 md:mb-7 lg:mb-8 xl:mb-9 tracking-wider rounded-lg border-light dark:border-dark shadow-neumo dark:shadow-neumo-dark">
+        <article className="px-3 sm:px-5 md:px-7 lg:px-16 pt-5 sm:pt-6 md:pt-7 lg:pt-14 pb-8 sm:pb-9 md:pb-12 lg:pb-20 mb-5 md:mb-7 lg:mb-8 xl:mb-9 rounded-lg border-light dark:border-dark shadow-neumo dark:shadow-neumo-dark">
           <h1 className="max-w-4xl mx-auto mb-4 md:mb-6 lg:mb-10 text-lg sm:text-2xl lg:text-3xl font-bold break-words table">
-            <span className="text-xl md:text-3xl lg:text-4xl text-accent dark:text-yellow-300">
+            <span className="text-xl sm:text-3xl lg:text-4xl text-accent dark:text-yellow-300">
               {post.title.substr(0, 1)}
             </span>
             {post.title.substr(1)}
@@ -84,49 +84,53 @@ const Post: NextPage<Props> = ({ config, option, post }) => {
               </div>
             </>
           ) : null}
-          <div className="my-4 md:my-6 flex flex-wrap justify-center items-center">
-            <Svg type="publish" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6" />
-            <time dateTime={post.publishedAt} itemProp="datepublished" className="ml-2 text-xs sm:text-sm block">
-              {getCorrectDate(post.publishedAt, 'yyyy年MM月dd日')}
-            </time>
-            <Svg type="update" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6 ml-4 sm:ml-14" />
-            <time dateTime={post.revisedAt} itemProp="modified" className="ml-2 text-xs sm:text-sm block">
-              {getCorrectDate(post.revisedAt, 'yyyy年MM月dd日')}
-            </time>
+          <div className="my-4 md:my-6 flex justify-center items-center">
+            <div className="my-2 md:my-0 flex flex-wrap justify-center items-center">
+              <Svg type="publish" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6" />
+              <time dateTime={post.publishedAt} itemProp="datepublished" className="ml-2 text-xs sm:text-sm block">
+                {getCorrectDate(post.publishedAt, 'yyyy年MM月dd日')}
+              </time>
+            </div>
+            <div className="my-2 md:my-0 flex flex-wrap justify-center items-center">
+              <Svg type="update" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6 ml-4 sm:ml-14" />
+              <time dateTime={post.revisedAt} itemProp="modified" className="ml-2 text-xs sm:text-sm block">
+                {getCorrectDate(post.revisedAt, 'yyyy年MM月dd日')}
+              </time>
+            </div>
           </div>
-          <div className="mx-auto my-4 md:my-6 table md:flex flex-wrap justify-center items-center">
+          <div className="mx-auto my-4 md:my-6 table md:flex justify-around items-center">
             <div className="flex justify-center items-center">
               <Svg type="category" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6" />
               <ul className="text-xs sm:text-sm flex items-center">
                 {post.categories.map((category) => (
                   <li
                     key={category.id}
-                    className={`ml-3 sm:ml-6 text-accent dark:text-yellow-300 rounded-sm sm:rounded border-light hover:border-accent dark:border-dark dark:hover:border-yellow-300 shadow-inset hover:shadow-none dark:shadow-inset-dark dark:hover:shadow-none`}
+                    className={`ml-3 sm:ml-5 text-accent dark:text-yellow-300 rounded-sm sm:rounded border-light hover:border-accent dark:border-dark dark:hover:border-yellow-300 shadow-inset hover:shadow-none dark:shadow-inset-dark dark:hover:shadow-none`}
                   >
                     <Link href={`/categories/${category.slug}`}>
-                      <a className="px-2.5 sm:px-5 py-1 sm:py-1.5 block">{category.title}</a>
+                      <a className="px-2.5 sm:px-4 py-1 sm:py-1.5 block">{category.title}</a>
                     </Link>
                   </li>
                 ))}
               </ul>
             </div>
-            <div className="mt-4 md:mt-0 md:ml-10 lg:ml-20 flex justify-center items-center">
+            <div className="mt-4 md:mt-0 flex justify-center items-center">
               <Svg type="tag" strokeWidth={1.5} class="w-5 h-5 sm:w-6 sm:h-6" />
               <ul className="text-xs sm:text-sm flex items-center">
                 {post.tags.map((tag) => (
                   <li
                     key={tag.id}
-                    className={`ml-3 sm:ml-6 text-accent dark:text-yellow-300 rounded-sm sm:rounded border-light hover:border-accent dark:border-dark dark:hover:border-yellow-300 shadow-inset hover:shadow-none dark:shadow-inset-dark dark:hover:shadow-none`}
+                    className={`ml-3 sm:ml-5 text-accent dark:text-yellow-300 rounded-sm sm:rounded border-light hover:border-accent dark:border-dark dark:hover:border-yellow-300 shadow-inset hover:shadow-none dark:shadow-inset-dark dark:hover:shadow-none`}
                   >
                     <Link href={`/tags/${tag.slug}`}>
-                      <a className="px-2.5 sm:px-5 py-1 sm:py-1.5 block">{tag.title}</a>
+                      <a className="px-2.5 sm:px-4 py-1 sm:py-1.5 block">{tag.title}</a>
                     </Link>
                   </li>
                 ))}
               </ul>
             </div>
           </div>
-          <div className="mt-8 lg:mt-10 prose prose-sm lg:prose max-w-none lg:max-w-none dark:prose-dark">{body}</div>
+          <div className="mt-8 lg:mt-14 prose prose-sm lg:prose max-w-none lg:max-w-none dark:prose-dark">{body}</div>
         </article>
       </Body>
     </>
