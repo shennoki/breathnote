aa9f3c4b2217b2d7c62ced86a15ce8bdd3bdea31
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-03-03 20:53:11 +0900

    refactor: Add WAI-ARIA to pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -29,36 +29,38 @@ const Pagination: FC<Props> = ({ allPostLength, pageType, offset, slug }) => {
 
   return (
     <>
-      <ul className="my-6 flex justify-center">
-        {range(1, quantity).map((num, index) => (
-          <React.Fragment key={index}>
-            {num === quantity && offset <= quantity - 3 ? (
-              <li key="predotted" className="py-1 md:py-2 leading-none">
-                ...
-              </li>
-            ) : null}
+      <nav aria-label="pagination" className="my-6">
+        <ul className="flex justify-center">
+          {range(1, quantity).map((num, index) => (
+            <React.Fragment key={index}>
+              {num === quantity && offset <= quantity - 3 ? (
+                <li key="predotted" className="py-1 md:py-2 leading-none">
+                  ...
+                </li>
+              ) : null}
 
-            {num === 1 || num === quantity || (num > offset - 2 && num < offset + 2) ? (
-              <li
-                key={num}
-                className={`mx-1 md:mx-2 border border-shadow-light dark:border-shadow-dark rounded-full ${
-                  num === offset ? `shadow-inset-light dark:shadow-inset-dark ` : null
-                }hover:border-shadow-light dark:hover:border-shadow-dark hover:shadow-inset-light dark:hover:shadow-inset-dark`}
-              >
-                <Link href={num === 1 ? rootPath : `${path}/${num}`}>
-                  <a className="px-3 md:px-4 py-1 md:py-2 rounded-full block">{num}</a>
-                </Link>
-              </li>
-            ) : null}
+              {num === 1 || num === quantity || (num > offset - 2 && num < offset + 2) ? (
+                <li
+                  key={num}
+                  className={`mx-1 md:mx-2 border border-shadow-light dark:border-shadow-dark rounded-full ${
+                    num === offset ? `shadow-inset-light dark:shadow-inset-dark ` : null
+                  }hover:border-shadow-light dark:hover:border-shadow-dark hover:shadow-inset-light dark:hover:shadow-inset-dark`}
+                >
+                  <Link href={num === 1 ? rootPath : `${path}/${num}`}>
+                    <a className="px-3 md:px-4 py-1 md:py-2 rounded-full block">{num}</a>
+                  </Link>
+                </li>
+              ) : null}
 
-            {num === 1 && offset >= 4 ? (
-              <li key="postdotted" className="py-1 md:py-2 leading-none">
-                ...
-              </li>
-            ) : null}
-          </React.Fragment>
-        ))}
-      </ul>
+              {num === 1 && offset >= 4 ? (
+                <li key="postdotted" className="py-1 md:py-2 leading-none">
+                  ...
+                </li>
+              ) : null}
+            </React.Fragment>
+          ))}
+        </ul>
+      </nav>
     </>
   )
 }
