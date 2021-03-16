f58d0aa597322331fb5b89f23b1def6714301ab0
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-03-17 05:04:35 +0900

    refactor: Disable next-pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const { createSecureHeaders } = require('next-secure-headers')
-const withPWA = require('next-pwa')
-const prod = process.env.NODE_ENV === 'production'
+// const withPWA = require('next-pwa')
+// const prod = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
+// module.exports = withPWA({
+module.exports = {
   // X-Powered-Byヘッダを削除
   poweredByHeader: false,
   // next/imageのsrcとして許可する外部ドメイン
@@ -19,8 +20,8 @@ module.exports = withPWA({
     ]
   },
   // net-pwa | https://github.com/GoogleChrome/workbox/issues/1790#issuecomment-620894023
-  pwa: {
-    disable: prod ? false : true,
-    dest: 'public',
-  },
-})
+  // pwa: {
+  //   disable: prod ? false : true,
+  //   dest: 'public',
+  // },
+}
