fb2f57ea507afd693c83348998d2760f14692294
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-04-18 08:28:56 +0900

    refactor: Optimize API

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,22 +1,19 @@
+import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
-import fetch from 'node-fetch'
 import { API_ENDPOINT, API_KEY, DRAFT_TOKEN } from 'utils/env'
 
-export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const KEY = { headers: { 'X-API-KEY': API_KEY } }
   const token = req.query.secret
 
   if (!token || token !== DRAFT_TOKEN) {
     res.writeHead(302, { Location: `/404` }).end()
   }
 
-  const draft = await fetch(`${API_ENDPOINT}/posts/${req.query.draftId}?fields=id&draftKey=${req.query.draftKey}`, {
-    headers: { 'X-API-KEY': API_KEY },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      alert(err)
-      res.status(500).end()
-    })
+  const draft = await axios
+    .get(`${API_ENDPOINT}/posts/${req.query.draftId}?fields=id&draftKey=${req.query.draftKey}`, KEY)
+    .then((res) => res.data)
+    .catch((err) => res.status(500).send(err))
 
   res.setPreviewData({
     draftId: draft.id,
