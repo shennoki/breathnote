0ac52ed3ff966b02bdf1c5315b88b0fffc012abb
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2021-05-11 16:16:08 +0900

    test: Update snapshots

--- test/pages/posts/slug.test.tsx
+++ test/pages/posts/slug.test.tsx
@@ -1,8 +1,6 @@
 import { cleanup, render } from '@testing-library/react'
-import renderToString from 'next-mdx-remote/render-to-string'
+import { serialize } from 'next-mdx-remote/serialize'
 import React from 'react'
-import CustomImage from '../../../src/components/atoms/CustomImage'
-import CustomLink from '../../../src/components/atoms/CustomLink'
 import { fetchAllPosts } from '../../../src/libs/store'
 import Post from '../../../src/pages/posts/[slug]'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -12,17 +10,11 @@ describe(`ARTICLE PAGE (pages/posts/[slug].tsx)`, () => {
   afterEach(cleanup)
 
   test('snapshot', async () => {
-    const components = {
-      a: CustomLink,
-      img: CustomImage,
-    }
-
     let post = (await fetchAllPosts()).contents.slice(-1)[0]
 
     post = {
       ...post,
-      body: await renderToString(post.body, {
-        components,
+      body: await serialize(post.body, {
         mdxOptions: {
           rehypePlugins: [rehypePrism],
         },
