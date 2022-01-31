b9e2eb6b0b12b6fc1064281eb947a13eed876b3f
Shinki <53631280+Code-Shinki@users.noreply.github.com>
2022-01-31 22:21:16 +0900

    style: Add filename to code block

--- src/components/molecules/PostBody.tsx
+++ src/components/molecules/PostBody.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './PostBody.module.scss'
 
 type Props = {
@@ -6,6 +6,19 @@ type Props = {
 }
 
 const PostBody: React.FC<Props> = ({ body }) => {
+  useEffect(() => {
+    // コードブロックのファイル名要素を移動
+    const root = document.getElementsByClassName('prism-root')[0]
+    const pre = root.getElementsByTagName('PRE')
+
+    Array.from(pre).forEach((elm) => {
+      const filenameElm = elm.previousElementSibling
+      if (filenameElm && filenameElm.classList.contains('codeblock-filename')) {
+        elm.appendChild(filenameElm)
+      }
+    })
+  }, [])
+
   return <div className={`${styles.post} prism-root`}>{body}</div>
 }
 
